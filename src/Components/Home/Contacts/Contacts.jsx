d136514ea033ae41b00de92cb9fa992f009828f2
Pasito-JS <Lavarbb@yandex.ru>
2025-01-15 21:45:21 +0300

    Adding a validate

--- src/Components/Home/Contacts/Contacts.jsx
+++ src/Components/Home/Contacts/Contacts.jsx
@@ -1,16 +1,42 @@
 import './Contacts.css';
+import {useState} from 'react';
 
 const Contacts = () => {
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [message, setMessage] = useState('');
+	const [errors, setErrors] = useState({});
 	
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		alert('Спасибо за обратную связь!')
+		
+		setErrors({});
+		
+		let formErrors = {};
+		if (!name) {
+			formErrors.name = 'Имя обязательно для заполнения.';
+		}
+		if (!email) {
+			formErrors.email = 'Email обязателен для заполнения.';
+		} else if (!/\S+@\S+\.\S+/.test(email)) {
+			formErrors.email = 'Введите корректный email.';
+		}
+		if (!message) {
+			formErrors.message = 'Сообщение обязательно для заполнения.';
+		}
+		
+		if (Object.keys(formErrors).length > 0) {
+			setErrors(formErrors);
+			return;
+		}
+		
+		alert('Спасибо за обратную связь!');
+		
 	};
 	
 	return (
 		<div id='contacts'>
 			<div className="container">
-				
 				<div className="refs__text">
 					<h2 className="title-2">Обратная связь:</h2>
 					<p>Оставьте свой вопрос в данном поле и мы обязательно с вами свяжемся!</p>
@@ -19,21 +45,39 @@ const Contacts = () => {
 				<div className="block-contact">
 					<form onSubmit={handleSubmit}>
 						<label htmlFor="name">Имя</label>
-						<input type="text" name="name" placeholder="Михаил"/>
+						<input
+							type="text"
+							name="name"
+							placeholder="Михаил"
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+							className={errors.name ? 'error' : 'white' }
+						/>
 						
 						<label htmlFor="email">Почта</label>
-						<input type="text" name="email" placeholder="example@gmail.com"/>
+						<input
+							type="text"
+							name="email"
+							placeholder="example@gmail.com"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+							className={errors.email ? 'error' : 'white' }
+						/>
 						
 						<label htmlFor="message">Сообщение</label>
-						<textarea name="message" placeholder='Сообщение:'></textarea>
+						<textarea
+							name="message"
+							placeholder='Сообщение:'
+							value={message}
+							onChange={(e) => setMessage(e.target.value)}
+							className={errors.message ? 'error' : 'white' }
+						></textarea>
 						
 						<button type='submit'>Отправить сообщение</button>
-					
 					</form>
 					
 					<img src="/AgroCulture_IMG/man_with_hoe.png" style={{transform: 'scaleX(-1)'}} alt="contact"/>
 				</div>
-			
 			</div>
 		</div>
 	);
